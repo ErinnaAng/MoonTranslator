af03236872191b1c21e6dbbff8e262a1f769840d
ErinnaAng <146809821+ErinnaAng@users.noreply.github.com>
2024-01-15 22:07:38 +0800

    Create dbconfig.php

---
+++ dbconfig.php
@@ -0,0 +1,13 @@
+<?php
+$HOST = "localhost";
+$DB = "moonuse";
+$USER = "root";
+$PASSWORD = "";
+
+//Koneksi PDO
+try {
+    $pdo = new PDO("mysql:host=" . $HOST . ";dbname=" . $DB, $USER, $PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database, please try again" . $DB . ": " . $e->getMessage());
+}
